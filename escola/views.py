5bedb7118f209c021a8f76692df523c7cab10b42
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-03-29 17:05:08 -0300

    att: CRUD and Authentication

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,37 @@
-from django.http import JsonResponse
+from rest_framework import viewsets, generics
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from escola.models import Aluno, Curso, Matricula
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1,'nome':'Lucas'}
-    return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo Alunos e Alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo Cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as Matriculas de um Aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando Alunos Matriculados"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+
